f04c55d01e223f7cb13bae8eb22e5235df18a944
doanhtu07 <doanhtu07@gmail.com>
2026-04-04 22:17:06 -0500

    Move some deps

--- tanstack-start-client-app/src/router.tsx
+++ tanstack-start-client-app/src/router.tsx
@@ -1,12 +1,8 @@
 import { createRouter } from '@tanstack/react-router'
-
-// Import the generated route tree
-
 import { QueryClient } from '@tanstack/react-query'
 import { setupRouterSsrQueryIntegration } from '@tanstack/react-router-ssr-query'
 import { routeTree } from './routeTree.gen'
 
-// Create a new router instance
 export const getRouter = () => {
   const queryClient = new QueryClient()
 
--- tanstack-start-client-app/vite.config.ts
+++ tanstack-start-client-app/vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
-
 import tailwindcss from '@tailwindcss/vite'
 import { nitro } from 'nitro/vite'
 import dotenv from 'dotenv'
