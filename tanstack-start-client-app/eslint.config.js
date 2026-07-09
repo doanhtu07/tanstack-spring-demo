43473823e14f0c7bae01fd4ed01015a4e700d354
doanhtu07 <doanhtu07@gmail.com>
2026-07-08 19:34:59 -0500

    chore: adjust eslint

--- tanstack-start-client-app/eslint.config.js
+++ tanstack-start-client-app/eslint.config.js
@@ -6,10 +6,17 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactPlugin from 'eslint-plugin-react'
 
 export default [
+  globalIgnores(['eslint.config.js', 'prettier.config.js', 'src/orval']),
+
   ...tanstackConfig,
   reactPlugin.configs.flat.recommended, // This is not a plugin object, but a shareable config object
   reactPlugin.configs.flat['jsx-runtime'], // Add this if you are using React 17+
   reactHooks.configs.flat.recommended,
 
-  globalIgnores(['eslint.config.js', 'prettier.config.js', 'src/orval']),
+  // MARK: React
+  {
+    rules: {
+      'react/prop-types': 'off',
+    },
+  },
 ]
